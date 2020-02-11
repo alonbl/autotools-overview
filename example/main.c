5d1a15c6a54ba83877febaa43005645e11d68d89
Alon Bar-Lev <alon.barlev@gmail.com>
2020-02-11 21:07:32 +0200

    Cross compile

--- example/main.c
+++ example/main.c
@@ -2,6 +2,10 @@
 #include <config.h>
 #endif
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #ifdef STDC_HEADERS
 #include <stdio.h>
 #endif
@@ -15,7 +19,13 @@
 int main() {
 
 #if ENABLE_DYNAMIC
+#ifdef _WIN32
+	LoadLibraryA("test.dll");
+#elif defined(HAVE_DLFCN_H)
 	dlopen("test.so", RTLD_NOW);
+#else
+#error Cannot determine dynamic method
+#endif
 #endif
 
 	printf("The value of '%s' is '%s'\n", "MY_VAR1", MY_VAR1);
