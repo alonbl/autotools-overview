1b3264ba2bf568254f77d0b7c252ec9f0ce4607e
Alon Bar-Lev <alon.barlev@gmail.com>
2020-02-11 20:04:40 +0200

    Basic autoconf

--- example/main.c
+++ example/main.c
@@ -1,8 +1,19 @@
+#ifdef STDC_HEADERS
 #include <stdio.h>
+#endif
+
+#ifdef HAVE_DLFCN_H
+#include <dlfcn.h>
+#endif
 
 #include "common.h"
 
 int main() {
+
+#if HAVE_DLFCN_H
+	dlopen("test.so", RTLD_NOW);
+#endif
+
 	printf("The value of '%s' is '%s'\n", "MY_VAR1", MY_VAR1);
 
 	common_foo();
